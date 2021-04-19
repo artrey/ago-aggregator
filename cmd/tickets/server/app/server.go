05199226d677a84cbb05e73ad6573be4e45617b8
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2021-04-19 17:48:26 +0300

    Simple stub server

--- cmd/tickets/server/app/server.go
+++ cmd/tickets/server/app/server.go
@@ -2,7 +2,9 @@ package app
 
 import (
 	ticketsV1Pb "github.com/artrey/ago-aggregator/pkg/api/proto/v1"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"log"
+	"time"
 )
 
 type Server struct {
@@ -15,5 +17,18 @@ func New() *Server {
 
 func (s *Server) Search(request *ticketsV1Pb.SearchRequest, server ticketsV1Pb.TicketsService_SearchServer) error {
 	log.Println(request)
+	for i := 0; i < 5; i++ {
+		time.Sleep(time.Second)
+		if err := server.Send(&ticketsV1Pb.SearchResponse{
+			Id:            int64(i + 1),
+			DepartureTime: timestamppb.New(time.Now()),
+			TravelTime:    &timestamppb.Timestamp{Seconds: int64(time.Hour * 2 / time.Second)},
+			Cost:          20_000_00,
+		}); err != nil {
+			log.Println(err)
+			return err
+		}
+	}
+	log.Println("Successfully finished")
 	return nil
 }
